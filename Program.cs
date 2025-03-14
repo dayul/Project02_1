ce0365d1d3174e171fbffe6ce5225ae33dca41b1
dayul <dyulchoo@naver.com>
2025-03-14 10:19:11 +0900

    #2 사칙연산 출력

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,26 @@ namespace Project02_1
     {
         static void Main(string[] args)
         {
+            // 나중에 입력 받을 예정
+            int a, b;
+
+            int result;
+            a = 100;
+            b = 50;
+
+            result = a + b;
+
+            // + : 문자열 연결 연산자(operator)
+            Console.WriteLine(a + "+" + b + "=" + result);
+
+            result = a - b;
+            Console.WriteLine(a + '-' + b + "=" + result);
+
+            result = a * b;
+            Console.WriteLine(a + '*' + b + "=" + result);
+
+            result = a / b;
+            Console.WriteLine(a + '/' + b + "=" + result);
         }
     }
 }
